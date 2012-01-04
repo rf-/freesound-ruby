b9f0e556e0eeaea9a574719284cebace25b2a37b
Alex Genco <alexgenco@gmail.com>
2012-01-04 16:22:37 -0500

    got rid of Api class

--- lib/freesound/api.rb
+++
@@ -1,8 +0,0 @@
-class Freesound::InvalidRequest < ArgumentError; end
-
-module Freesound::API
-  def self.get(request)
-    raise(InvalidRequest, "#{request.inspect} is not a #{Request.inspect}") unless request.is_a?(Request)
-
-  end
-end
